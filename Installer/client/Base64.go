2f64dd4ac88fca500df98ca9b83437163cceea74
thewginstaller <laksowtfgvns@outlook.com>
2022-09-28 23:35:14 +0330

    A

---
+++ Installer/client/Base64.go
@@ -0,0 +1,24 @@
+package main
+
+import "os"
+import "fmt"
+import "encoding/base64"
+
+func PEMDecoder( PemFileBase64Str string ) []uint8 {
+    PEMFileObj ,_ := base64.RawStdEncoding.DecodeString( PemFileBase64Str )
+    return PEMFileObj
+}
+
+func PEMEncoder( PemFileName string ) string {
+    PEMFileObj , _ := os.ReadFile( PemFileName )
+    PEMFileObjBase64Str := base64.RawStdEncoding.EncodeToString( PEMFileObj )
+    return PEMFileObjBase64Str
+}
+
+func main() {
+    CertObjBase64 := PEMEncoder( "Client-TLS-ED25519.crt" )
+    KeyObjBase64 := PEMEncoder( "Client-TLS-ED25519.key" )
+    CertObj := PEMDecoder( CertObjBase64 )
+    KeyObj := PEMDecoder( KeyObjBase64 )
+}
+
